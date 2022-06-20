73cb0dd9abdb8b91b47383b4ce295bea3af009d2
ilknur <ilknurmduman60@gmail.com>
2022-06-20 23:37:39 +0300

    Proje dosyası ekle.

---
+++ Burç Tarihleri/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Burç_Tarihleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(165, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Bul";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(134, 118);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(185, 36);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(87, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Doğum Tarihinizi Giriniz :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(454, 310);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Burcunu Öğren";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Burç Tarihleri/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Burç_Tarihleri
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ay, gün;
+            ay = Convert.ToInt16(dateTimePicker1.Value.Month);
+            gün = Convert.ToInt16(dateTimePicker1.Value.Day);
+
+            if ((ay == 3 && gün >= 21) || (ay == 4 && gün <= 20))
+                MessageBox.Show("Koç Burcusunuz");
+
+            if ((ay == 4 && gün >= 21) || (ay == 5 && gün <= 21))
+                MessageBox.Show("Boğa Burcusunuz");
+
+            if ((ay == 5 && gün >= 22) || (ay == 6 && gün <= 22))
+                MessageBox.Show("İkizler Burcusunuz");
+
+            if ((ay == 6 && gün >= 23) || (ay == 7 && gün <= 22))
+                MessageBox.Show("Yengeç Burcusunuz");
+
+            if ((ay == 7 && gün >= 23) || (ay == 8 && gün <= 22))
+                MessageBox.Show("Aslan Burcusunuz");
+
+            if ((ay == 8 && gün >= 23) || (ay == 9 && gün <= 22))
+                MessageBox.Show("Başak Burcusunuz");
+
+            if ((ay == 9 && gün > 23) || (ay == 10 && gün <= 22))
+                MessageBox.Show("Terazi Burcusunuz");
+
+            if ((ay == 10 && gün > 23) || (ay == 11 && gün <= 21))
+                MessageBox.Show("Akrep Burcusunuz");
+
+            if ((ay == 11 && gün > 22) || (ay == 12 && gün <= 21))
+                MessageBox.Show("Yay Burcusunuz");
+
+            if ((ay == 12 && gün > 22) || (ay == 1 && gün <= 21))
+                MessageBox.Show("Oğlak Burcusunuz");
+
+            if ((ay == 1 && gün > 22) || (ay == 2 && gün <= 19))
+                MessageBox.Show("Kova Burcusunuz");
+
+            if ((ay == 2 && gün > 20) || (ay == 3 && gün <= 20))
+                MessageBox.Show("Balık Burcusunuz");
+
+
+        }
+    }
+}
